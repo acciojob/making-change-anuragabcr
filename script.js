27b8f959d78808fda3e5561213e7263eb5f5c826
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-12 11:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const makeChange = (c) => {
   // your name here
+	curr = {
+        q: 25,
+        d: 10,
+        n: 5,
+        p: 1
+    }
+    mon = {
+        q: 0,
+        d: 0,
+        n: 0,
+        p: 0
+    }
+    for (const key of curr) {
+        while(c >= curr[key]) {
+            mon[key] += 1
+            c -= curr[key]
+        }
+    }
+    return mon;
 };
 
 // Do not the change the code below
